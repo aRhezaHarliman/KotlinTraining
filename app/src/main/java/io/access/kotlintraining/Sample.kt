fc2c865e0be5fcf66108f23a6c4238497b5e75e4
aRhezaHarliman <rheza.harliman@access-company.com>
2018-04-19 17:27:29 +0900

    Exercise 3 guard

--- app/src/main/java/io/access/kotlintraining/Sample.kt
+++ app/src/main/java/io/access/kotlintraining/Sample.kt
@@ -185,4 +185,41 @@ fun nabeatsuConverter(i: Int) {
     }
 }
 
+/*
+* Exercise 3
+* */
 
+/*
+* Java
+*
+*
+* public void sendMessageToClient(
+    @Nullable Client client,
+    @Nullable String message,
+    @NonNull Mailer mailer) {
+    if (client == null || message == null) return;
+
+    PersonalInfo personalInfo = client.getPersonalInfo();
+    if (personalInfo == null) return;
+
+    String email = personalInfo.getEmail();
+    if (email == null) return;
+
+    mailer.sendMessage(email, message);
+}
+* */
+
+fun sendMessageToClient(client: Client?, message: String?, mailer: Mailer) {
+    if (message == null) return
+    val personalInfo = client?.personalInfo
+    var email = personalInfo?.email?.let {
+        mailer.sendMessage(it, message)
+    }
+}
+
+// コンパイル通すための諸々
+class Client (val personalInfo: PersonalInfo?)
+class PersonalInfo (val email: String?)
+interface Mailer {
+    fun sendMessage(email: String, message: String)
+}
