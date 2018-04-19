1d11bd407f43e26852175ccd9dc97e8d40ad8ec8
aRhezaHarliman <rheza.harliman@access-company.com>
2018-04-19 10:41:18 +0900

    Add more FizzBuzz test for Fizz, Buzz and other case

--- app/src/test/java/io/access/kotlintraining/SampleTest.kt
+++ app/src/test/java/io/access/kotlintraining/SampleTest.kt
@@ -26,15 +26,47 @@ class SampleTest {
 
     @Test
     fun testFizzBuzz() {
+        // FizzBuzz test
         val fizzBuzz = fizzBuzz(90)
         val expected = "FizzBuzz!"
-        assertEquals(expected, fizzBuzz, "[want] $expected [got] $fizzBuzz")
+        assertEquals(expected, fizzBuzz, "FizzBuzz test [want] $expected [got] $fizzBuzz")
+
+        // Fizz test
+        val fizz = fizzBuzz(9)
+        val expectedFizz = "Fizz!"
+        assertEquals(expectedFizz, fizz, "Fizz test [want] $expectedFizz [got] $fizz")
+
+        // Buzz test
+        val buzz = fizzBuzz(10)
+        val expectedBuzz = "Buzz!"
+        assertEquals(expectedBuzz, buzz, "Buzz test [want] $expectedBuzz [got] $buzz")
+
+        // other
+        val other = fizzBuzz(0)
+        val expectedOther = "0"
+        assertEquals(expectedBuzz, buzz, "Other test [want] $expectedOther [got] $other")
     }
 
     @Test
     fun testWhenFizzBuzz() {
+        // FizzBuzz test
         val fizzBuzz = whenFizzBuzz(90)
         val expected = "FizzBuzz!"
-        assertEquals(expected, fizzBuzz, "[want] $expected [got] $fizzBuzz")
+        assertEquals(expected, fizzBuzz, "FizzBuzz test [want] $expected [got] $fizzBuzz")
+
+        // Fizz test
+        val fizz = whenFizzBuzz(9)
+        val expectedFizz = "Fizz!"
+        assertEquals(expectedFizz, fizz, "Fizz test [want] $expectedFizz [got] $fizz")
+
+        // Buzz test
+        val buzz = whenFizzBuzz(10)
+        val expectedBuzz = "Buzz!"
+        assertEquals(expectedBuzz, buzz, "Buzz test [want] $expectedBuzz [got] $buzz")
+
+        // other
+        val other = whenFizzBuzz(0)
+        val expectedOther = "0"
+        assertEquals(expectedBuzz, buzz, "Other test [want] $expectedOther [got] $other")
     }
 }
